56bffcb5c870b5a1cd6e9808890a0c6f01545279
Emanuel Mhlanga <Emanuelmhlanga55@gmail.com>
2024-01-16 17:16:02 +0300

    Finally Got the navigation right!!😁

--- Training/components/HomeScreen.js
+++ Training/components/HomeScreen.js
@@ -1,6 +1,8 @@
-import { SafeAreaView } from "react-native/types";
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function HomeScreen() {
+export default function HomeScreen({navigation}) {
     <SafeAreaView style={styles.container}>
         <Text styles={styles.textStyles}>This is the HomePage!</Text>
     </SafeAreaView>
