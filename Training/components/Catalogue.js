c6ccaed9a19a65e76e0ba7af6480bcf9083ffa52
Emanuel Mhlanga <Emanuelmhlanga55@gmail.com>
2024-01-17 23:10:09 +0300

    Need to Add the sound extension tommorow

--- Training/components/Catalogue.js
+++ Training/components/Catalogue.js
@@ -1,38 +1,73 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import { ScrollView, SafeAreaView } from "react-native";
-import { Card, Text, Button } from "react-native-paper";
+import { ScrollView, SafeAreaView, Image, TouchableOpacity, StyleSheet, ImageBackground } from "react-native";
+import { Text, Button } from "react-native-paper";
+import { Card } from "@rneui/base";
 
 export default function Catalogue() {
     const navigation = useNavigation();
 
-    return(<SafeAreaView>
-          <ScrollView>
+    return(
+        // Background Image
+        <ImageBackground 
+        style={styles.container}
+        source={require("../Assets/img/HomepageBg.jpg")}
+        >
+            {/* Scrollable view Takes 90% of the Screen */}
+          <ScrollView style={{height: '90%'}}>
+
+            {/* Card 1 */}
+            <TouchableOpacity>
             <Card>
-                <Card.Title mode={'contained'} title={'Melodic Trap beat'} subtitle={'prod by EmzaPharaohBeats'}>
-                
-                    <Card.Content>
-                        <Text variant="titleLarge">Track No.1</Text>
-                        <Text variant="bodyMedium">Track No.1</Text>
-                    </Card.Content>
-                    <Card.Cover  source={{uri: '../Assets/img/Melodic.jpg'}} style={{width:100, height: 100}}/>
-                    
+                <Card.Title>
+                    Melodic Trap Beat 
                 </Card.Title>
+            <Card.Image source={require('../Assets/img/Melodic.jpg')} style={{width: '100%', height: 300, justifyContent: 'center'}} />
+            <Text variant="titleMedium">probucer - EmzaPharaohBeats</Text>
+            <Text variant="titleSmall">BPM - 130</Text>
+
+
             </Card>
+            </TouchableOpacity>
+
+            {/* Card 2 */}
+            <TouchableOpacity>
+            <Card>
+                <Card.Title>
+                    Monte Booker Type Beat 
+                </Card.Title>
+            <Card.Image source={require('../Assets/img/MonteBooker.jpg')} style={{width: '100%', height: 300, justifyContent: 'center'}} />
+            <Text variant="titleMedium">probucer - EmzaPharaohBeats</Text>
+            <Text variant="titleSmall">BPM - 130</Text>
+            </Card>
+
+            </TouchableOpacity>
+            
+           
 
         </ScrollView>
 
         <Button 
         mode="contained"
-        style={{width: 350}}
+        style={{width: 350, marginTop: 0, marginLeft: 'auto', marginRight: 'auto'}}
         onPress={() => navigation.navigate('Home')}
         >
         Back
         </Button>
 
-    </SafeAreaView>
+   </ImageBackground>
       
 
     )
 
     }
+
+const styles = StyleSheet.create({
+
+    cardContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+    }
+})
